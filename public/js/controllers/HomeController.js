1f3ef7ac4e85bcdca1ad525b577ab1a48c91dade
Jonatan Souza <jonatangd.souza@gmail.com>
2016-07-28 20:55:58 -0300

    time out implemented

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -1,22 +1,41 @@
 angular.module('charon').controller('HomeController',
-    function($scope, $routeParams, $http, $location) {
+    function($scope, $routeParams, $http, $location, $interval) {
+        $scope.charonAPI = false;
         $scope.project = "Charon";
-        $http({
-            method: 'GET',
-            url: 'http://localhost:3000/api/openstack/version'
-        }).then(function(data) {
-            $scope.version = data.data;
-        }, function(err) {
-            console.log(err);
-        });
+        $scope.charonLocate = 'http://localhost:3000/api/openstack/'
 
-        $http({
-            method: 'GET',
-            url: 'http://localhost:3000/api/openstack/limits'
-        }).then(function(data) {
-            $scope.limits = data.data;
-        }, function(err) {
-            console.log(err);
-        });
+        connectingCharon();
+        if (!$scope.charonAPI) {
+            var promisse = $interval(function() {
+                if (!$scope.charonAPI) {
+                    console.log('attemp ');
+                    connectingCharon();
+                } else {
+                    $interval.cancel(promisse);
+                }
+            }, 7000, 5);
+        }
 
+
+        function connectingCharon() {
+            $http({
+                method: 'GET',
+                url: 'http://localhost:3000/api/openstack/version'
+            }).then(function(data) {
+                $scope.charonAPI = true;
+                $scope.version = data.data;
+            }, function(err) {
+                console.log(err);
+            });
+
+            $http({
+                method: 'GET',
+                url: 'http://localhost:3000/api/openstack/limits'
+            }).then(function(data) {
+                $scope.charonAPI = true;
+                $scope.limits = data.data;
+            }, function(err) {
+                console.log(err);
+            });
+        }
     });
